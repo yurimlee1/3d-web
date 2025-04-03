8804068bf6741e4b0b3d3adda66c776f43726f9b
yurimlee1 <yurim.lee.123@gmail.com>
2025-04-03 13:58:07 -0400

    uploaded project

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 font-generalsans'>
                     Hi, I am Yurim<span className='waving-hand'>👋</span>
                 </p>
-                <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
+                <p className='hero_tag text-gray_gradient'>See How My Computer Moves!</p>
             </div>
 
             <div className='w-full h-full absolute inset-0'>
